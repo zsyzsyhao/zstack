661d74cf5bd39bae4f05c979cc454aa8bfcea623
luchukun <luchukun1993@gmail.com>
2016-11-04 19:56:07 +0800

    revert KVMConstant.java

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMConstant.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMConstant.java
@@ -10,43 +10,48 @@ public interface KVMConstant {
     @PythonClass
     String KVM_HYPERVISOR_TYPE = "KVM";
 
-    public static final String KVM_CONNECT_PATH = "/host/connect";
-    public static final String KVM_PING_PATH = "/host/ping";
-    public static final String KVM_ECHO_PATH = "/host/echo";
-    public static final String KVM_CHECK_PHYSICAL_NETWORK_INTERFACE_PATH = "/network/checkphysicalnetworkinterface";
-    public static final String KVM_HOST_CAPACITY_PATH = "/host/capacity";
-    public static final String KVM_HOST_FACT_PATH = "/host/fact";
-    public static final String KVM_REALIZE_L2NOVLAN_NETWORK_PATH = "/network/l2novlan/createbridge";
-    public static final String KVM_CHECK_L2NOVLAN_NETWORK_PATH = "/network/l2novlan/checkbridge";
-    public static final String KVM_REALIZE_L2VLAN_NETWORK_PATH = "/network/l2vlan/createbridge";
-    public static final String KVM_CHECK_L2VLAN_NETWORK_PATH = "/network/l2vlan/checkbridge";
-    public static final String KVM_ATTACH_ISO_PATH = "/vm/iso/attach";
-    public static final String KVM_DETACH_ISO_PATH = "/vm/iso/detach";
-    public static final String KVM_START_VM_PATH = "/vm/start";
-    public static final String KVM_STOP_VM_PATH = "/vm/stop";
-    public static final String KVM_SUSPEND_VM_PATH = "/vm/suspend";
-    public static final String KVM_RESUME_VM_PATH = "/vm/resume";
-    public static final String KVM_REBOOT_VM_PATH = "/vm/reboot";
-    public static final String KVM_DESTROY_VM_PATH = "/vm/destroy";
-    public static final String KVM_MIGRATE_VM_PATH = "/vm/migrate";
-    public static final String KVM_GET_VNC_PORT_PATH = "/vm/getvncport";
-    public static final String KVM_VM_CHANGE_CPUMEMORY = "/vm/changecpumem";
-    public static final String KVM_VM_SYNC_PATH = "/vm/vmsync";
-    public static final String KVM_ATTACH_VOLUME = "/vm/attachdatavolume";
-    public static final String KVM_DETACH_VOLUME = "/vm/detachdatavolume";
-    public static final String KVM_ATTACH_NIC_PATH = "/vm/attachnic";
-    public static final String KVM_DETACH_NIC_PATH = "/vm/detachnic";
-    public static final String KVM_VM_CHECK_STATE = "/vm/checkstate";
-    public static final String KVM_TAKE_VOLUME_SNAPSHOT_PATH = "/vm/volume/takesnapshot";
-    public static final String KVM_MERGE_SNAPSHOT_PATH = "/vm/volume/mergesnapshot";
-    public static final String KVM_LOGOUT_ISCSI_PATH = "/iscsi/target/logout";
-    public static final String KVM_LOGIN_ISCSI_PATH = "/iscsi/target/login";
-    public static final String KVM_HARDEN_CONSOLE_PATH = "/vm/console/harden";
-    public static final String KVM_DELETE_CONSOLE_FIREWALL_PATH = "/vm/console/deletefirewall";
-    public static final String ISO_TO = "kvm.isoto";
-    public static final String ANSIBLE_PLAYBOOK_NAME = "kvm.py";
-    public static final String ANSIBLE_MODULE_PATH = "ansible/kvm";
 
+
+
+
+    String KVM_CONNECT_PATH = "/host/connect";
+    String KVM_PING_PATH = "/host/ping";
+    String KVM_ECHO_PATH = "/host/echo";
+    String KVM_CHECK_PHYSICAL_NETWORK_INTERFACE_PATH = "/network/checkphysicalnetworkinterface";
+    String KVM_HOST_CAPACITY_PATH = "/host/capacity";
+    String KVM_HOST_FACT_PATH = "/host/fact";
+    String KVM_REALIZE_L2NOVLAN_NETWORK_PATH = "/network/l2novlan/createbridge";
+    String KVM_CHECK_L2NOVLAN_NETWORK_PATH = "/network/l2novlan/checkbridge";
+    String KVM_REALIZE_L2VLAN_NETWORK_PATH = "/network/l2vlan/createbridge";
+    String KVM_CHECK_L2VLAN_NETWORK_PATH = "/network/l2vlan/checkbridge";
+    String KVM_ATTACH_ISO_PATH = "/vm/iso/attach";
+    String KVM_DETACH_ISO_PATH = "/vm/iso/detach";
+    String KVM_START_VM_PATH = "/vm/start";
+    String KVM_STOP_VM_PATH = "/vm/stop";
+    String KVM_SUSPEND_VM_PATH = "/vm/suspend";
+    String KVM_RESUME_VM_PATH = "/vm/resume";
+    String KVM_REBOOT_VM_PATH = "/vm/reboot";
+    String KVM_DESTROY_VM_PATH = "/vm/destroy";
+    String KVM_MIGRATE_VM_PATH = "/vm/migrate";
+    String KVM_GET_VNC_PORT_PATH = "/vm/getvncport";
+    String KVM_VM_CHANGE_CPUMEMORY = "/vm/changecpumem";
+    String KVM_VM_SYNC_PATH = "/vm/vmsync";
+    String KVM_ATTACH_VOLUME = "/vm/attachdatavolume";
+    String KVM_DETACH_VOLUME = "/vm/detachdatavolume";
+    String KVM_ATTACH_NIC_PATH = "/vm/attachnic";
+    String KVM_DETACH_NIC_PATH = "/vm/detachnic";
+    String KVM_VM_CHECK_STATE = "/vm/checkstate";
+    String KVM_VM_CHANGE_PASSWORD_PATH = "/vm/changepasswd";
+    String KVM_VM_SET_ROOT_PASSWORD_PATH = "/vm/setrootpasswd";
+    String KVM_TAKE_VOLUME_SNAPSHOT_PATH = "/vm/volume/takesnapshot";
+    String KVM_MERGE_SNAPSHOT_PATH = "/vm/volume/mergesnapshot";
+    String KVM_LOGOUT_ISCSI_PATH = "/iscsi/target/logout";
+    String KVM_LOGIN_ISCSI_PATH = "/iscsi/target/login";
+    String KVM_HARDEN_CONSOLE_PATH = "/vm/console/harden";
+    String KVM_DELETE_CONSOLE_FIREWALL_PATH = "/vm/console/deletefirewall";
+    String ISO_TO = "kvm.isoto";
+    String ANSIBLE_PLAYBOOK_NAME = "kvm.py";
+    String ANSIBLE_MODULE_PATH = "ansible/kvm";
 
     String MIN_LIBVIRT_LIVESNAPSHOT_VERSION = "1.0.0";
     String MIN_QEMU_LIVESNAPSHOT_VERSION = "1.3.0";
